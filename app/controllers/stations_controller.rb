d6f10068a8d5e981e80c01425d82c44cba925c09
Cristian Correa <cristian.correa.capetillo@gmail.com>
2024-11-16 08:25:42 -0300

    stations_fix_description_field

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,47 +1,54 @@
 class StationsController < ApplicationController
+  before_action :set_station, only: [:show, :edit, :update, :destroy]
+
   def index
     @stations = Station.all
   end
 
   def show
-    @station = Station.find(params[:id])
   end
 
   def new
     @station = Station.new
+    @blocks = Block.all
+  end
+
+  def edit
+    @blocks = Block.all
   end
 
   def create
     @station = Station.new(station_params)
+
     if @station.save
       redirect_to @station, notice: 'Station was successfully created.'
     else
+      @blocks = Block.all
       render :new
     end
   end
 
-  def edit
-    @station = Station.find(params[:id])
-  end
-
   def update
-    @station = Station.find(params[:id])
     if @station.update(station_params)
       redirect_to @station, notice: 'Station was successfully updated.'
     else
+      @blocks = Block.all
       render :edit
     end
   end
 
   def destroy
-    @station = Station.find(params[:id])
     @station.destroy
     redirect_to stations_url, notice: 'Station was successfully destroyed.'
   end
 
   private
 
+  def set_station
+    @station = Station.find(params[:id])
+  end
+
   def station_params
-    params.require(:station).permit(:name, metro_line_ids: [])
+    params.require(:station).permit(:name, :description, block_ids: [])
   end
 end
--- app/models/block.rb
+++ app/models/block.rb
@@ -1,8 +1,9 @@
 class Block < ApplicationRecord
   has_and_belongs_to_many :stations
+  has_and_belongs_to_many :walls
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :priority, inclusion: { in: %w[Low Medium High] }
-  validates :status, inclusion: { in: %w[To\ Do In\ Progress Done] }
+  validates :priority, presence: true
+  validates :status, presence: true
 end
